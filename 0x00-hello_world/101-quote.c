9157a5d0925db17ec1fdbeb23693776c8918094c
bashmi586 <bashmi586@gmial.com>
2023-02-18 04:10:07 -0800

    intel

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdio.h>
+
+/**
+ * main - main file
+ *
+ * Return: 1 successful
+ */
+
+int main(void)
+{       
+        fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",stdout);
+	return (1);
+}
